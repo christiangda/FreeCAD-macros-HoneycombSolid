7250d0d49d7d44cb3c87412f259048eec2c39fae
Christian González Di Antonio <christiangda@gmail.com>
2021-01-30 20:44:09 +0100

    refactored to satisfy https://github.com/FreeCAD/FreeCAD-macros/pull/89

--- honeycomb_solid/honeycomb_solid.py
+++ honeycomb_solid/honeycomb_solid.py
@@ -3,8 +3,6 @@ HoneycombSolid --> Honeycomb solid creator.
 (c) 2021 Christian González Di Antonio <christiangda@gmail.com>
 """
 
-__Version__ = "v1.0.3"
-
 import FreeCAD
 import Part
 import math 
@@ -12,7 +10,7 @@ from FreeCAD import Base
 
 class HoneycombSolid:
 
-	def __init__(self, obj):
+	def __init__(self, obj, version):
 		''' Custom properties of Honeycomb feature '''
 		obj.addProperty("App::PropertyLength","Length","Honeycomb","Length of the Honeycomb").Length=100.0
 		obj.addProperty("App::PropertyLength","Width","Honeycomb","Width of the Honeycomb").Width=100.0
@@ -23,7 +21,7 @@ class HoneycombSolid:
 		obj.Proxy = self
 	
 		self.Type = 'HoneycombSolid'
-		self.Version = __version__
+		self.Version = version
         
 
 	def onChanged(self, fp, prop):
@@ -53,7 +51,7 @@ class HoneycombSolid:
 		m.rotateZ(edges_angle)
 		v=Base.Vector(radius,0,0)
 
-		for i in range(edges):
+		for _ in range(edges):
 			figure.append(v)
 			v = m.multiply(v)
 
@@ -82,7 +80,7 @@ class HoneycombSolid:
 		for column in range(-n_cols, n_cols):
 			for row in range(-n_rows, n_rows):
 				if (column == 0) and (row == 0):
-    				continue
+					continue
 				else:
 					if column % 2 != 0:
 					# odd
@@ -138,7 +136,7 @@ class HoneycombSolid:
 
 	def __setstate__(self,state):
 		''' When restoring the pickled object from document we have the chance to set some
-		internals here. Since no data were pickled nothing needs to be done here.
+		internals here.
 		'''
 		self.Type = state[0]
 		self.Version = state[1]
@@ -182,29 +180,77 @@ class ViewProviderHoneycombSolid:
 		return """
 			/* XPM */
 			static const char * ViewProviderHoneycombSolid_xpm[] = {
-			"16 16 6 1",
+			"16 16 54 1",
 			" 	c None",
-			".	c #141010",
-			"+	c #615BD2",
-			"@	c #C39D55",
-			"#	c #000000",
-			"$	c #57C355",
-			"        ........",
-			"   ......++..+..",
-			"   .@@@@.++..++.",
-			"   .@@@@.++..++.",
-			"   .@@  .++++++.",
-			"  ..@@  .++..++.",
-			"###@@@@ .++..++.",
-			"##$.@@$#.++++++.",
-			"#$#$.$$$........",
-			"#$$#######      ",
-			"#$$#$$$$$#      ",
-			"#$$#$$$$$#      ",
-			"#$$#$$$$$#      ",
-			" #$#$$$$$#      ",
-			"  ##$$$$$#      ",
-			"   #######      "};
+			".	c #181818",
+			"+	c #5F5F5F",
+			"@	c #636363",
+			"#	c #353535",
+			"$	c #474747",
+			"%	c #434343",
+			"&	c #606060",
+			"*	c #424242",
+			"=	c #111111",
+			"-	c #444444",
+			";	c #151515",
+			">	c #3F3F3F",
+			",	c #1E1E1E",
+			"'	c #1C1C1C",
+			")	c #1B1B1B",
+			"!	c #2C2C2C",
+			"~	c #535353",
+			"{	c #0A0A0A",
+			"]	c #363636",
+			"^	c #383838",
+			"/	c #2F2F2F",
+			"(	c #252525",
+			"_	c #555555",
+			":	c #393939",
+			"<	c #515151",
+			"[	c #262626",
+			"}	c #161616",
+			"|	c #464646",
+			"1	c #4F4F4F",
+			"2	c #545454",
+			"3	c #3A3A3A",
+			"4	c #131313",
+			"5	c #121212",
+			"6	c #5E5E5E",
+			"7	c #0C0C0C",
+			"8	c #0F0F0F",
+			"9	c #0B0B0B",
+			"0	c #0D0D0D",
+			"a	c #1D1D1D",
+			"b	c #292929",
+			"c	c #3D3D3D",
+			"d	c #222222",
+			"e	c #171717",
+			"f	c #1A1A1A",
+			"g	c #282828",
+			"h	c #272727",
+			"i	c #5A5A5A",
+			"j	c #3C3C3C",
+			"k	c #595959",
+			"l	c #616161",
+			"m	c #505050",
+			"n	c #2E2E2E",
+			"o	c #565656",
+			"   .+@@   #$    ",
+			"    %@&* =-$    ",
+			"    ;>,')!~&    ",
+			"    {]^  /@@    ",
+			"@   ($$   _@@   ",
+			"@   :$    /@@   ",
+			"<][}|1     2~3['",
+			"445#6@     78889",
+			"%  '&@@    #$   ",
+			"$   $@@   0-$   ",
+			"    a&@+  b$$   ",
+			"     c*[de>_    ",
+			"@    fgh {i@@   ",
+			"@    ^$   j@@   ",
+			"@@  4|$   {k@@  ",
+			"lm ,n$     ]@o^["};
 			"""
 
 	def __getstate__(self):
@@ -221,13 +267,13 @@ class ViewProviderHoneycombSolid:
 		return None
 
 
-def makeHoneycombSolid():
+def makeHoneycombSolid(version):
 	doc = FreeCAD.activeDocument()
 
 	if doc is None:
 		doc = FreeCAD.newDocument()
 
 	obj = doc.addObject("Part::FeaturePython", "HoneycombSolid")
-	HoneycombSolid(obj)
+	HoneycombSolid(obj,version)
 	ViewProviderHoneycombSolid(obj.ViewObject)
 	doc.recompute()
